69ed889311dd919851318f5010087f255b3cbc86
Ryan James <rcjames1004@gmail.com>
2019-09-08 18:45:01 -0700

    Initial commit

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+/// <summary>
+/// Creates an asset bundle for model.blend
+/// </summary>
+public class CreateAssetBundle {
+
+	[MenuItem("Build/Build Asset Bundle")]
+	static void Build()
+	{
+        // Create an asset bundle 
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles/WindowsStandalone", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles/WindowsStore", BuildAssetBundleOptions.None, BuildTarget.WSAPlayer);
+		BuildPipeline.BuildAssetBundles("Assets/AssetBundles/Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # unity-asset-bundle-builder
-Builds Unity asset bundles.
+Builds multiple asset bundles from an FBX file.
+
+## Setup
+1. Download Unity 2019.1.0f2
+2. Open unity and login
+
+## Running
+1. Copy your fbx file and associated resources to `Assets/Model/model.fbx`
+2. Build the asset bundles `/path/to/Unity -projectPath . -quit -batchmode -executeMethod CreateAssetBundle.Build`
+
+To clean up run `git clean -df`
